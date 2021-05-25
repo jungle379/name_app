97871fb188dbfe8a62f0a0a8c2775d0d033a26c8
jungle379 <jungle379@gmail.com>
2021-05-26 05:50:30 +0900

    ⚡️

---
+++ front/tailwind.config.js
@@ -0,0 +1,8 @@
+module.exports = {
+  mode: "jit",
+  purge: ["./src/**/*.{js,jsx}"],
+  darkMode: false, // 'media' or 'class'
+  theme: { extend: {} },
+  variants: { extend: {} },
+  plugins: [],
+};
